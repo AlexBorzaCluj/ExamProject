41c708cac7e7dc93431d24e92a4199bac93094da
Alexandru Borza <alec.borza@gmail.com>
2024-01-22 19:09:17 +0200

    Initial commit

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,40 @@
+
+package pages;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BasePage {
+    public static WebDriver driver;
+    private String BASE_URL =
+            "https://ecommerce-playground.lambdatest.io/";
+
+    public BasePage() {
+// Default constructor
+    }
+
+    public BasePage(WebDriver driver) {
+        BasePage.driver = driver;
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+}
